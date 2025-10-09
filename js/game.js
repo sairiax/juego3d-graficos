39633d01f126b03c5b0bdb5d7b393be03a536adc
Sergi Cortes <sergicortes@Sergis-MacBook-Air.local>
2025-10-09 16:21:01 +0200

    cambios finales

--- js/game.js
+++ js/game.js
@@ -441,11 +441,13 @@ function init() {
     window.addEventListener('resize', updateAspectRatio);
 
     // GUI Setup
-    var gui = new dat.GUI();
-    gui.domElement.style.position = 'absolute';
-    gui.domElement.style.bottom = '-500px';
-    gui.domElement.style.right = '10px';
-    gui.domElement.style.zIndex = '1000';
+    // GUI Setup
+var gui = new dat.GUI({ autoPlace: false });  // Desactiva el posicionamiento automático
+document.body.appendChild(gui.domElement);    // Añádelo al body para que sea global
+gui.domElement.style.position = 'fixed';      // Fijo al viewport
+gui.domElement.style.bottom = '5%';         // Margen inferior de 10px
+gui.domElement.style.right = '10px';          // Margen derecho de 10px
+gui.domElement.style.zIndex = '1000';         // Encima de otros elementos
 
     // Planet Folder
     var guiPlanet = gui.addFolder('Planet');
